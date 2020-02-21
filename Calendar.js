768cdcad7d87cff4087bbee7ee07db4d78ac0b92
tae <taeshindev@gmail.com>
2020-02-21 11:39:36 +0900

    calendar 셀렉터 변경
    datepicker와 구분을 위해서 calendar로 변경

--- Calendar.js
+++ Calendar.js
@@ -9,29 +9,29 @@ const lastDatesOfMonth = new Date(today.getFullYear(), today.getMonth()+1, 0);
 const lastDate = lastDatesOfMonth.getDate();
 const lastDay = lastDatesOfMonth.getDay();
 
-const renderDays = () => day.map((v)=><div className="datepicker__cell">{v}</div>)
+const renderDays = () => day.map((v)=><div className="calendar__cell">{v}</div>)
 
 const renderDates = () => {
     return [ 
-        new Array(firstDay).fill(null).map(_=><div className="datepicker__cell"></div>),
-        new Array(lastDate).fill(null).map((_, i)=><div className="datepicker__cell">{i+1}</div>),
-        new Array(6 - lastDay).fill(null).map(_=><div className="datepicker__cell"></div>)
+        new Array(firstDay).fill(null).map(_=><div className="calendar__cell"></div>),
+        new Array(lastDate).fill(null).map((_, i)=><div className="calendar__cell">{i+1}</div>),
+        new Array(6 - lastDay).fill(null).map(_=><div className="calendar__cell"></div>)
     ];
 }
 
 function Calendar() {
-    return <div className="datepicker__container">
-        <div className="datepicker__indicator">
-            <button className="datepicker__navigator datepicker__navigator--prev-month"></button>
+    return <div className="calendar__container">
+        <div className="calendar__indicator">
+            <button className="calendar__navigator calendar__navigator--prev-month"></button>
                 {
                     today.getFullYear() + '.' + (today.getMonth()+1)
                 }
-            <button className="datepicker__navigator datepicker__navigator--next-month"></button>
+            <button className="calendar__navigator calendar__navigator--next-month"></button>
         </div>
-        <div className="datepicker__header">
+        <div className="calendar__header">
             { renderDays() }
         </div>
-        <div className="datepicker__body">
+        <div className="calendar__body">
             { renderDates() }
         </div>
     </div>
