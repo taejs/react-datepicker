57577589885280cdbbbfe1bf39e3ba0087d5f8b0
tae <taeshindev@gmail.com>
2020-02-27 14:17:36 +0900

    DateValue 생성으로 인한 기존 코드 삭제

--- useDates.js
+++ useDates.js
@@ -1,22 +1,11 @@
 import React, { useState } from 'react';
 
-function useDates() {
-    const [selectedDate, setSelectedDate] = useState(new Date());
-    const firstDatesOfMonth = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), 1);
-    const lastDatesOfMonth = new Date(selectedDate.getFullYear(), selectedDate.getMonth()+1, 0);
+function useDates(initialDate) {
+    const [currentDate, setCurrentDate] = useState(initialDate);
 
     return [
-        {
-            dateTime : selectedDate,
-            year: selectedDate.getFullYear(),
-            month : selectedDate.getMonth() + 1,
-            date : selectedDate.getDate(),
-            day : selectedDate.getDay(),
-            firstDay : firstDatesOfMonth.getDay(),
-            lastDate : lastDatesOfMonth.getDate(),
-            lastDay : lastDatesOfMonth.getDay()
-        },
-        setSelectedDate
+        currentDate,
+        setCurrentDate
     ]
 
 }
