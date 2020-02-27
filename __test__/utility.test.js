f5499ea7a50d8e38009511bd6ce956e03c8cabef
tae <taeshindev@gmail.com>
2020-02-27 14:10:30 +0900

    DateValu 생성으로 인한 테스트 수정

--- __test__/utility.test.js
+++ __test__/utility.test.js
@@ -1,8 +1,9 @@
 import { test } from '../qunit';
-import dateToString from '../utilities';
+import {dateToString} from '../utilities';
+import DateValue from '../DateValue';
 
 test('function dateToString should return string following given format', function(assert) {
-    const d = new Date(2020, 02, 29);
+    const d = new DateValue(2020, 2, 29)._nativeDateObject;
     assert.equal(dateToString(d), '2020-02-29', 'default format is YYYY-MM-DD');
     assert.equal(dateToString(d, 'YYYY-MM-DD'), '2020-02-29', 'YYYY-MM-DD');
     assert.equal(dateToString(d, 'YYYY.MM.DD'), '2020.02.29', 'YYYY.MM.DD');
