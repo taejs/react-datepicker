d7892cd89071651d1408be77cb7d90c62ad8f91e
tae <taeshindev@gmail.com>
2020-02-27 14:19:03 +0900

    DateValue 생성으로 인한 코드 수정

--- utilities.js
+++ utilities.js
@@ -1,25 +1,26 @@
 const padNumber = (number, targetLength) => {
-    const stringified = (new Array(targetLength).fill(0) + number).slice(-number.length);
-    return parseInt(stringified);
+    const stringified = (new Array(targetLength).fill(0).join('') + number).slice(-targetLength);
+    return stringified;
 }
-
-const formatFunctions = {
-    'YYYY' : (date) => date.getFullYear(),
-    'MM' : (date) => padNumber(date.getMonth() + 1, 2),
-    'DD' : (date) => padNumber(date.getDate(), 2)
-};
-
-const formattingTokens = new RegExp(Object.keys(formatFunctions).join('|'));
 
 //format is ISO8601 Extended format
 export function dateToString(date, format) {
-    if(date === undefined) throw error('first parameter : date is required');
-    if(!date instanceof Date) throw error('first parameter : date should be internal Date object');
+    const formatFunctions = {
+        'YYYY' : () => date.year,
+        'MM' : () => padNumber(date.month, 2),
+        'DD' : () => padNumber(date.date, 2)
+    };
+    const formattingTokens = new RegExp(Object.keys(formatFunctions).join('|'), 'g');
+    console.log(formattingTokens);
+    let output;
+    if(!date) throw Error('first parameter : date is required');
+    if(!date instanceof Date) throw Error('first parameter : date should be internal Date object');
     format = format || 'YYYY-MM-DD';
+    output = format;
     
     let array = format.match(formattingTokens);
-    for(let i=0; i<length; i++) {
-        output = format.replace(new RegExp(array[i]), formatFunctions[array[i]]);
+    for(let i=0; i<array.length; i++) {
+        output = output.replace(new RegExp(array[i], 'g'), formatFunctions[array[i]]);
     }
 
     return output;
